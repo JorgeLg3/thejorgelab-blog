669e2c6064bc014850bc5b0a65132bbdf64c74dd
jorge <jorgelopezgamarra@gmail.com>
2026-05-17 21:19:35 +0200

    add auth basic logic

--- django_project/urls.py
+++ django_project/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("pages.urls")),
     path("health/", include("healthcheck.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ]
