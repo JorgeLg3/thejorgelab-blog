4e03966eb2ac53a04736b7a53724657987e577b1
jorge <jorgelopezgamarra@gmail.com>
2026-05-19 21:22:44 +0200

    add post create, update and delete views and templates

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -10,3 +12,36 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "blog/post_create.html"
+    fields = ["title", "body", "tags"]
+
+    # automatically get author from login details
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostUpdate(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = "blog/post_update.html"
+    fields = ["title", "body", "tags"]
+
+    # UserPassesTestMixin check: compares author with user
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+
+
+class PostDelete(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_delete.html"
+    success_url = reverse_lazy("home")
+
+    # UserPassesTestMixin check: compares author with user
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
--- django_project/urls.py
+++ django_project/urls.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("pages.urls")),
     path("health/", include("healthcheck.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("posts/", include("blog.urls")),
     path("admin/", admin.site.urls),
+    path("", include("pages.urls")),
 ]
