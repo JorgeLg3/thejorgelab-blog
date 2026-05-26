6dee681adf49ae8de95e833899cb3eef0461c9bf
jorge <jorgelopezgamarra@gmail.com>
2026-05-26 22:47:34 +0200

    update urls to remove unnecessary home page

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete
+from .views import PostDetail, PostCreate, PostUpdate, PostDelete
 
 urlpatterns = [
     path("new/", PostCreate.as_view(), name="post_create"),
     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
     path("<int:pk>/edit/", PostUpdate.as_view(), name="post_update"),
     path("<int:pk>/delete/", PostDelete.as_view(), name="post_delete"),
-    path("", PostList.as_view(), name="post_list"),
-    path("tag/<str:tag_name>", PostList.as_view(), name="post_list_tag"),
 ]
--- django_project/urls.py
+++ django_project/urls.py
@@ -2,10 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
+from blog.views import PostList
 
 urlpatterns = [
     path("health/", include("healthcheck.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", PostList.as_view(), name="home"),
+    path("tag/<str:tag_name>", PostList.as_view(), name="post_list_tag"),
+    path("posts/", RedirectView.as_view(pattern_name="home", permanent=True)),
     path("posts/", include("blog.urls")),
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
