1176d5aa003129aba2721a1f575bc16a1b95b7f6
jorge <jorgelopezgamarra@gmail.com>
2026-05-27 17:56:07 +0200

    fix about tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,12 +1,7 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 
 
-class ViewTests(SimpleTestCase):
-    def test_home_page(self):
-        """Home page should respond with a success 200."""
-        response = self.client.get("/", follow=True)
-        self.assertEqual(response.status_code, 200)
-
+class ViewTests(TestCase):
     def test_about_page(self):
         """About page should respond with a success 200."""
         response = self.client.get("/about", follow=True)
